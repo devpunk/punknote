4272566aba8e2461668e794ca1b754ca1b6ed292
vauxhall <zero.griffin@gmail.com>
2017-06-01 19:50:27 -0500

    controller view main protocol

--- punknote/Controller/Main/Controller.swift
+++ punknote/Controller/Main/Controller.swift
@@ -1,6 +1,46 @@
 import UIKit
 
-class Controller:UIViewController
+class Controller<T:UIView>:UIViewController
 {
+    init()
+    {
+        super.init(nibName:nil, bundle:nil)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func loadView()
+    {
+        guard
+            
+            let viewMain:ViewMain.Type = T as? ViewMain.Type
+        else
+        {
+            return
+        }
+        let view:T = T(controller:self)
+        self.view = view
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        edgesForExtendedLayout = UIRectEdge()
+        extendedLayoutIncludesOpaqueBars = false
+        automaticallyAdjustsScrollViewInsets = false
+    }
+    
+    override var preferredStatusBarStyle:UIStatusBarStyle
+    {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override var prefersStatusBarHidden:Bool
+    {
+        return false
+    }
 }
--- punknote/View/Main/ViewMain.swift
+++ punknote/View/Main/ViewMain.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewMain:UIView
+class ViewMain<T:UIViewController>:UIView
 {
     
 }
