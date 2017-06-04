00348eda68ff5eff68f4acb3f50b561f70ca14ff
vauxhall <zero.griffin@gmail.com>
2017-06-04 17:57:10 -0500

    model create on controller create

--- punknote/Controller/Create/CCreate.swift
+++ punknote/Controller/Create/CCreate.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class CCreate:Controller<VCreate>
 {
+    let model:MCreate
     
+    override init()
+    {
+        model = MCreate()
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
