18a7ff177b628651f20b48e4f15a8fab48a7b413
vauxhall <zero.griffin@gmail.com>
2017-06-04 17:50:30 -0500

    init model frame

--- punknote/Model/Create/MCreate.swift
+++ punknote/Model/Create/MCreate.swift
@@ -2,5 +2,13 @@ import Foundation
 
 class MCreate
 {
+    var frames:[MCreateFrame]
+    var selectedFrame:Int
     
+    init()
+    {
+        let startingFrame:MCreateFrame = MCreateFrame()
+        frames = [startingFrame]
+        selectedFrame = 0
+    }
 }
