a8e38384d9ce1203b64e2d6ccd408627c50afd01
vauxhall <zero.griffin@gmail.com>
2017-06-03 21:47:16 -0500

    refactor viewModel for controller

--- punknote/View/Abstract/View.swift
+++ punknote/View/Abstract/View.swift
@@ -2,20 +2,20 @@ import UIKit
 
 class View:UIView
 {
-    private(set) weak var viewModel:UIViewController!
+    private(set) weak var controller:UIViewController!
     weak var layoutLeft:NSLayoutConstraint!
     weak var layoutRight:NSLayoutConstraint!
     weak var layoutTop:NSLayoutConstraint!
     weak var layoutBottom:NSLayoutConstraint!
     weak var pushBackground:VPushBackground?
     
-    required init(viewModel:UIViewController)
+    required init(controller:UIViewController)
     {
         super.init(frame:CGRect.zero)
         clipsToBounds = true
         backgroundColor = UIColor.white
         translatesAutoresizingMaskIntoConstraints = false
-        self.viewModel = viewModel
+        self.controller = controller
     }
     
     required init?(coder:NSCoder)
