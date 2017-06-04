a9e0bf8300a32d97853ba74ceffb584351b4eaaa
vauxhall <zero.griffin@gmail.com>
2017-06-04 17:56:17 -0500

    frame properties

--- punknote/Model/Create/MCreateFrame.swift
+++ punknote/Model/Create/MCreateFrame.swift
@@ -2,5 +2,13 @@ import Foundation
 
 class MCreateFrame
 {
+    var title:String
+    var duration:TimeInterval
+    private let kDefaultDuration:TimeInterval = 3
     
+    init()
+    {
+        title = ""
+        duration = kDefaultDuration
+    }
 }
