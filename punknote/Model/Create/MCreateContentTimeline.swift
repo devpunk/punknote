abfefd393d10984579b5176977c40b125291263d
vauxhall <zero.griffin@gmail.com>
2017-06-05 09:53:47 -0500

    model create content protocol

---
+++ punknote/Model/Create/MCreateContentTimeline.swift
@@ -0,0 +1,9 @@
+//
+//  MCreateContentTimeline.swift
+//  punknote
+//
+//  Created by zero on 6/5/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
--- punknote/View/Create/VCreateCell.swift
+++ punknote/View/Create/VCreateCell.swift
@@ -2,5 +2,15 @@ import UIKit
 
 class VCreateCell:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
