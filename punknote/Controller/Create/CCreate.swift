8b77a90c407b3ce29f410da5ac60b1cd5de7bec6
vauxhall <zero.griffin@gmail.com>
2017-06-05 18:32:00 -0500

    footer button

--- punknote/Controller/Create/CCreate.swift
+++ punknote/Controller/Create/CCreate.swift
@@ -35,4 +35,8 @@ class CCreate:Controller<VCreate>
     {
         
     }
+    
+    func addFrame()
+    {
+    }
 }
--- punknote/View/Create/VCreateCellTimelineFooter.swift
+++ punknote/View/Create/VCreateCellTimelineFooter.swift
@@ -2,15 +2,53 @@ import UIKit
 
 class VCreateCellTimelineFooter:UICollectionReusableView
 {
+    private weak var controller:CCreate?
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.setImage(
+            self,
+            for:UIControlState.normal)
+        button.setImage(
+            <#T##image: UIImage?##UIImage?#>,
+            for:UIControlState.highlighted)
+        button.imageView!.clipsToBounds = true
+        button.imageView!.contentMode = UIViewContentMode.center
+        button.addTarget(
+            self,
+            action:#selector(actionAdd(sender:)),
+            for:UIControlEvents.touchUpInside)
+
+        addSubview(button)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: actions
+    
+    func actionAdd(sender button:UIButton)
+    {
+        controller?.addFrame()
+    }
+    
+    //MARK: public
+    
+    func config(controller:CCreate)
+    {
+        self.controller = controller
     }
 }
