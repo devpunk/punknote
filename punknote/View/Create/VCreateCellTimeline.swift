a2156c62e966088a3f0c8aa5a0bc10093952c132
vauxhall <zero.griffin@gmail.com>
2017-06-05 10:41:06 -0500

    cell timeline collection

--- punknote/View/Create/VCreateCellTimeline.swift
+++ punknote/View/Create/VCreateCellTimeline.swift
@@ -1,5 +1,94 @@
 import UIKit
 
-class VCreateCellTimeline:VCreateCell
+class VCreateCellTimeline:VCreateCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
+    private weak var collectionView:VCollection!
+    private let kInterItem:CGFloat = 2
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VCreateTimelineCell.self)
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+            flow.minimumInteritemSpacing = kInterItem
+            flow.minimumLineSpacing = kInterItem
+            flow.sectionInset = UIEdgeInsets(
+                top:0,
+                left:kInterItem,
+                bottom:0,
+                right:kInterItem)
+        }
+        
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func layoutSubviews()
+    {
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+        super.layoutSubviews()
+    }
+    
+    override func config(controller:CCreate, model:MCreateContentProtocol)
+    {
+        super.config(controller:controller, model:model)
+        collectionView.reloadData()
+    }
+    
+    //MARK: collectionView delegate
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        let height:CGFloat = collectionView.bounds.maxY
+        let size:CGSize = CGSize(width:height, height:height)
+        
+        return size
+    }
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        guard
+            
+            let count:Int = controller?.model.frames.count
+        
+        else
+        {
+            return 0
+        }
+        
+        return count
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:VCreateTimelineCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:
+            VCreateTimelineCell.reusableIdentifier,
+            for:indexPath) as! VCreateTimelineCell
+        
+        return cell
+    }
 }
