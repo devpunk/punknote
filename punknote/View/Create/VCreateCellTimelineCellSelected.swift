8d5f9bb4401db49d6ae13baeb6fa573c88bf9271
vauxhall <zero.griffin@gmail.com>
2017-06-05 16:01:59 -0500

    model create selected

--- punknote/View/Create/VCreateCellTimelineCellSelected.swift
+++ punknote/View/Create/VCreateCellTimelineCellSelected.swift
@@ -2,5 +2,37 @@ import UIKit
 
 class VCreateCellTimelineCellSelected:UIView
 {
+    private let kCircleMargin:CGFloat = 3
     
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        isUserInteractionEnabled = false
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func draw(_ rect:CGRect)
+    {
+        guard
+        
+            let context:CGContext = UIGraphicsGetCurrentContext()
+        
+        else
+        {
+            return
+        }
+        
+        let width:CGFloat = rect.size.width
+        let width_2:CGFloat = width / 2.0
+        let width_2_margin:CGFloat = width_2 - kCircleMargin
+        let centerPoint:CGPoint = CGPoint(x:width_2, y:width_2)
+        
+    }
 }
