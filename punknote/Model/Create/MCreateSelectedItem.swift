44a364b6653d50ebd68ae8e236cdf0a13a43ab73
vauxhall <zero.griffin@gmail.com>
2017-06-05 16:50:02 -0500

    creating random items

--- punknote/Model/Create/MCreateSelectedItem.swift
+++ punknote/Model/Create/MCreateSelectedItem.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class MCreateSelectedItem
 {
+    let startingRad:CGFloat
+    let endingRad:CGFloat
     
+    init(startingRad:CGFloat, endingRad:CGFloat)
+    {
+        self.startingRad = startingRad
+        self.endingRad = endingRad
+    }
 }
