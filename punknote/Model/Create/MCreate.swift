2a8cdce2eba825a940374d0fcbaa0d69e0f1ffd9
vauxhall <zero.griffin@gmail.com>
2017-06-05 10:01:44 -0500

    content in model create

--- punknote/Model/Create/MCreate.swift
+++ punknote/Model/Create/MCreate.swift
@@ -2,13 +2,18 @@ import Foundation
 
 class MCreate
 {
+    let content:[MCreateContentProtocol]
     var frames:[MCreateFrame]
     var selectedFrame:Int
     
     init()
     {
         let startingFrame:MCreateFrame = MCreateFrame()
+        let contentTimeline:MCreateContentTimeline = MCreateContentTimeline()
+        
         frames = [startingFrame]
         selectedFrame = 0
+        content = [
+            contentTimeline]
     }
 }
