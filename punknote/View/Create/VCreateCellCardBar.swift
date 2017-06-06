101edc334524fe7786c5bde26a185c4273141e57
vauxhall <zero.griffin@gmail.com>
2017-06-06 17:56:58 -0500

    button clear on car bar

--- punknote/View/Create/VCreateCellCardBar.swift
+++ punknote/View/Create/VCreateCellCardBar.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class VCreateCellCardBar:UIView
 {
-    private let kButtonRight:CGFloat = -7
+    private let kButtonHorizontalMargin:CGFloat = 7
     private let kButtonWidth:CGFloat = 90
     private let kButtonVerticalMargin:CGFloat = 7
     private let kButtonCornerRadius:CGFloat = 9
@@ -22,42 +22,77 @@ class VCreateCellCardBar:UIView
         
         let border:VBorder = VBorder(color:UIColor(white:0, alpha:0.1))
         
-        let button:UIButton = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor.white
-        button.clipsToBounds = true
-        button.setTitle(
-            NSLocalizedString("VCreateCellCardBar_titleButton", comment:""),
+        let buttonDone:UIButton = UIButton()
+        buttonDone.translatesAutoresizingMaskIntoConstraints = false
+        buttonDone.backgroundColor = UIColor.white
+        buttonDone.clipsToBounds = true
+        buttonDone.setTitle(
+            NSLocalizedString("VCreateCellCardBar_titleButtonDone", comment:""),
             for:UIControlState.normal)
-        button.setTitleColor(
+        buttonDone.setTitleColor(
             UIColor.punkPurple,
             for:UIControlState.normal)
-        button.setTitleColor(
+        buttonDone.setTitleColor(
             UIColor.punkPurple.withAlphaComponent(0.2),
             for:UIControlState.highlighted)
-        button.titleLabel!.font = UIFont.bold(size:13)
-        button.layer.cornerRadius = kButtonCornerRadius
-        button.layer.borderColor = UIColor.punkPurple.cgColor
-        button.layer.borderWidth = kButtonBorderWidth
-        button.addTarget(
+        buttonDone.titleLabel!.font = UIFont.bold(size:13)
+        buttonDone.layer.cornerRadius = kButtonCornerRadius
+        buttonDone.layer.borderColor = UIColor.punkPurple.cgColor
+        buttonDone.layer.borderWidth = kButtonBorderWidth
+        buttonDone.addTarget(
             self,
-            action:#selector(actionButton(sender:)),
+            action:#selector(actionButtonDone(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        let buttonClear:UIButton = UIButton()
+        buttonClear.translatesAutoresizingMaskIntoConstraints = false
+        buttonClear.backgroundColor = UIColor.white
+        buttonClear.clipsToBounds = true
+        buttonClear.setTitle(
+            NSLocalizedString("VCreateCellCardBar_titleButtonClear", comment:""),
+            for:UIControlState.normal)
+        buttonClear.setTitleColor(
+            UIColor.punkPurple,
+            for:UIControlState.normal)
+        buttonClear.setTitleColor(
+            UIColor.punkPurple.withAlphaComponent(0.2),
+            for:UIControlState.highlighted)
+        buttonClear.titleLabel!.font = UIFont.bold(size:13)
+        buttonClear.layer.cornerRadius = kButtonCornerRadius
+        buttonClear.layer.borderColor = UIColor.punkPurple.cgColor
+        buttonClear.layer.borderWidth = kButtonBorderWidth
+        buttonClear.addTarget(
+            self,
+            action:#selector(actionButtonClear(sender:)),
             for:UIControlEvents.touchUpInside)
         
         addSubview(blur)
         addSubview(border)
-        addSubview(button)
+        addSubview(buttonClear)
+        addSubview(buttonDone)
         
         NSLayoutConstraint.equalsVertical(
-            view:button,
+            view:buttonDone,
             toView:self,
             margin:kButtonVerticalMargin)
         NSLayoutConstraint.rightToRight(
-            view:button,
+            view:buttonDone,
             toView:self,
-            constant:kButtonRight)
+            constant:-kButtonHorizontalMargin)
         NSLayoutConstraint.width(
-            view:button,
+            view:buttonDone,
+            constant:kButtonWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:buttonClear,
+            toView:self,
+            margin:kButtonVerticalMargin)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonClear,
+            toView:self,
+            constant:kButtonHorizontalMargin)
+        NSLayoutConstraint.width(
+            view:buttonClear,
             constant:kButtonWidth)
         
         NSLayoutConstraint.topToTop(
@@ -82,8 +117,13 @@ class VCreateCellCardBar:UIView
     
     //MARK: actions
     
-    func actionButton(sender button:UIButton)
+    func actionButtonDone(sender button:UIButton)
     {
         UIApplication.shared.keyWindow!.endEditing(true)
+    }
+    
+    func actionButtonClear(sender button:UIButton)
+    {
+        
     }
 }
