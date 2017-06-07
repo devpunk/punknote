22b3631abd644da8e3a91b2e3b6f549faa7836b9
vauxhall <zero.griffin@gmail.com>
2017-06-07 15:27:54 -0500

    layout background

--- punknote/View/Create/VCreateCellBackgroundCell.swift
+++ punknote/View/Create/VCreateCellBackgroundCell.swift
@@ -4,21 +4,19 @@ class VCreateCellBackgroundCell:UICollectionViewCell
 {
     private weak var background:UIView?
     private weak var imageIndicator:UIImageView!
-    private let kCornerRadius:CGFloat = 4
     
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
-        layer.cornerRadius = kCornerRadius
-        layer.borderColor = UIColor.black.cgColor
         
         let imageIndicator:UIImageView = UIImageView()
         imageIndicator.translatesAutoresizingMaskIntoConstraints = false
         imageIndicator.clipsToBounds = true
         imageIndicator.isUserInteractionEnabled = false
         imageIndicator.contentMode = UIViewContentMode.bottomRight
+        imageIndicator.image = #imageLiteral(resourceName: "assetGenericCreateSelected")
         self.imageIndicator = imageIndicator
         
         addSubview(imageIndicator)
@@ -55,13 +53,13 @@ class VCreateCellBackgroundCell:UICollectionViewCell
     {
         if isSelected || isHighlighted
         {
-            layer.borderWidth = 2
-            imageIndicator.image = #imageLiteral(resourceName: "assetGenericCreateSelectedOn")
+            background?.layer.borderWidth = 2
+            imageIndicator.isHidden = false
         }
         else
         {
-            layer.borderWidth = 0
-            imageIndicator.image = #imageLiteral(resourceName: "assetGenericCreateSelectedOff")
+            background?.layer.borderWidth = 0
+            imageIndicator.isHidden = true
         }
     }
     
@@ -71,14 +69,29 @@ class VCreateCellBackgroundCell:UICollectionViewCell
     {
         self.background?.removeFromSuperview()
         
+        let width:CGFloat = bounds.maxX
+        let height:CGFloat = bounds.maxY
+        let height_2:CGFloat = height / 2.0
+        let remainWidth:CGFloat = width - height
+        let marginLeft:CGFloat = remainWidth / 2.0
+        
         let background:UIView = model.view()
+        background.layer.borderColor = UIColor.black.cgColor
+        background.layer.cornerRadius = height_2
         self.background = background
         
         insertSubview(background, at:0)
         
-        NSLayoutConstraint.equals(
+        NSLayoutConstraint.equalsVertical(
             view:background,
             toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:background,
+            toView:self,
+            constant:marginLeft)
+        NSLayoutConstraint.width(
+            view:background,
+            constant:height)
         
         hover()
     }
