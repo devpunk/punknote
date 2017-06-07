e823f8a3e231a0b5b3e74639631ac7f2a3e7ed30
vauxhall <zero.griffin@gmail.com>
2017-06-07 17:29:31 -0500

    duration slider bar

---
+++ punknote/View/Create/VCreateCellDurationSliderBar.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class VCreateCellDurationSliderBar:UIView
+{
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        isUserInteractionEnabled = false
+        
+        let blur:VBlur = VBlur.light()
+        
+        addSubview(blur)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
