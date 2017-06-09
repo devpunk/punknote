c4a373b7eab50d0b5203db00350a6c39fc55f764
vauxhall <zero.griffin@gmail.com>
2017-06-09 11:37:42 -0500

    spinner assets

--- punknote/View/Basic/VSpinner.swift
+++ punknote/View/Basic/VSpinner.swift
@@ -9,10 +9,7 @@ class VSpinner:UIImageView
         super.init(frame:CGRect.zero)
         
         let images:[UIImage] = [
-            #imageLiteral(resourceName: "assetSpinner0"),
-            #imageLiteral(resourceName: "assetSpinner1"),
-            #imageLiteral(resourceName: "assetSpinner2"),
-            #imageLiteral(resourceName: "assetSpinner3")
+            #imageLiteral(resourceName: "assetSpinner0")
         ]
         
         isUserInteractionEnabled = false
