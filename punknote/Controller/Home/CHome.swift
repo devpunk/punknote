cd515075582e2e327b466c2b71a58e9da3d7be8d
vauxhall <zero.griffin@gmail.com>
2017-06-09 12:42:13 -0500

    controller home load session

--- punknote/Controller/Home/CHome.swift
+++ punknote/Controller/Home/CHome.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class CHome:Controller<VHome>
 {
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        MSession.sharedInstance.loadSession()
+    }
 }
