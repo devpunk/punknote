9f96c8d4029b4107e22ec26f7a29dc5a9049c023
vauxhall <zero.griffin@gmail.com>
2017-06-09 12:47:14 -0500

    view spinner in home

--- punknote/View/Home/VHome.swift
+++ punknote/View/Home/VHome.swift
@@ -2,5 +2,29 @@ import UIKit
 
 class VHome:View
 {
+    private weak var spinner:VSpinner!
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        let spinner:VSpinner = VSpinner()
+        self.spinner = spinner
+        
+        addSubview(spinner)
+        
+        NSLayoutConstraint.equals(
+            view:spinner,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    deinit
+    {
+        spinner.stopAnimating()
+    }
 }
