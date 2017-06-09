cfff0386fdaaaa964ceb2218dfbc288e8125ed15
vauxhall <zero.griffin@gmail.com>
2017-06-09 14:13:22 -0500

    button new

--- punknote/View/Home/VHomeBar.swift
+++ punknote/View/Home/VHomeBar.swift
@@ -3,7 +3,13 @@ import UIKit
 class VHomeBar:UIView
 {
     private weak var controller:CHome!
-    private let kContentTop:CGFloat = 20
+    private let kIconTop:CGFloat = 20
+    private let kBorderHeight:CGFloat = 1
+    private let kCornerRadius:CGFloat = 5
+    private let kNewWidth:CGFloat = 80
+    private let kNewHeight:CGFloat = 32
+    private let kNewRight:CGFloat = -10
+    private let kNewTop:CGFloat = 32
     
     init(controller:CHome)
     {
@@ -13,6 +19,8 @@ class VHomeBar:UIView
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
         
+        let border:VBorder = VBorder(color:UIColor(white:0, alpha:0.2))
+        
         let imageIcon:UIImageView = UIImageView()
         imageIcon.isUserInteractionEnabled = false
         imageIcon.translatesAutoresizingMaskIntoConstraints = false
@@ -20,22 +28,76 @@ class VHomeBar:UIView
         imageIcon.clipsToBounds = true
         imageIcon.image = #imageLiteral(resourceName: "assetGenericSmallIcon")
         
+        let buttonNew:UIButton = UIButton()
+        buttonNew.translatesAutoresizingMaskIntoConstraints = false
+        buttonNew.clipsToBounds = true
+        buttonNew.backgroundColor = UIColor.punkPurple
+        buttonNew.layer.cornerRadius = kCornerRadius
+        buttonNew.setTitle(
+            NSLocalizedString("VHomeBar_buttonNew", comment:""),
+            for:UIControlState.normal)
+        buttonNew.setTitleColor(
+            UIColor.white,
+            for:UIControlState.normal)
+        buttonNew.setTitleColor(
+            UIColor(white:1, alpha:0.2),
+            for:UIControlState.highlighted)
+        buttonNew.titleLabel!.font = UIFont.bold(size:14)
+        buttonNew.addTarget(
+            self,
+            action:#selector(actionNew(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
         addSubview(imageIcon)
+        addSubview(border)
+        addSubview(buttonNew)
         
         NSLayoutConstraint.topToTop(
             view:imageIcon,
             toView:self,
-            constant:kContentTop)
+            constant:kIconTop)
         NSLayoutConstraint.bottomToBottom(
             view:imageIcon,
             toView:self)
         NSLayoutConstraint.equalsHorizontal(
             view:imageIcon,
             toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:border,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonNew,
+            toView:self,
+            constant:kNewTop)
+        NSLayoutConstraint.height(
+            view:buttonNew,
+            constant:kNewHeight)
+        NSLayoutConstraint.rightToRight(
+            view:buttonNew,
+            toView:self,
+            constant:kNewRight)
+        NSLayoutConstraint.width(
+            view:buttonNew,
+            constant:kNewWidth)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: actions
+    
+    func actionNew(sender button:UIButton)
+    {
+        
     }
 }
