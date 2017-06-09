7e76719ea4766ff902e391cf0db9602b061be474
vauxhall <zero.griffin@gmail.com>
2017-06-09 18:34:00 -0500

    home cell action

---
+++ punknote/View/Home/VHomeCellAction.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class VHomeCellAction:UICollectionViewCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config()
+    {
+        
+    }
+}
