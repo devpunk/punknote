1fc67f51f388930054d064ab18161a33c308c5b9
vauxhall <zero.griffin@gmail.com>
2017-07-23 01:08:12 -0500

    Remove unused code

--- punknote/Model/Share/MShareImage.swift
+++ punknote/Model/Share/MShareImage.swift
@@ -1,16 +1,4 @@
 import UIKit
-/*
-class MShareImage
-{
-    let image:UIImage
-    let duration:TimeInterval
-    
-    init(image:UIImage, duration:TimeInterval)
-    {
-        self.image = image
-        self.duration = duration
-    }
-}*/
 
 struct MShareImage
 {
